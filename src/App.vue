3eb273000d9e76f5ab8354712460a0a0dd9f05ae
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-07-20 20:24:23 -0500

    add login/logout authentications

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> */\*
-      <router-link to="/about">About</router-link> */\*
-      <router-link to="/test">Test</router-link>
+      <router-link to="/">Home</router-link> */_\*
+      <router-link to="/about">About</router-link> */_\*
+      <router-link to="/test">Test</router-link> */_\*
+      <router-link to="/signup">Signup</router-link> */_\*
+      <router-link to="/login">Login</router-link> */_\*
+      <router-link to="/logout">Logout</router-link>
     </div>
     <router-view />
   </div>
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <h3>{{ "This is Amazing" }}</h3>
   </div>
 </template>
 
